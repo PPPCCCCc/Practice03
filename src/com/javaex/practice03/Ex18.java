989d5510aee7c9aaa4b4d7f73577171bddab3cde
qkrcksgmlxd <qkrcksgmlxd@gmail.com>
2022-04-19 17:18:00 +0900

    1

--- src/com/javaex/practice03/Ex18.java
+++ src/com/javaex/practice03/Ex18.java
@@ -1,10 +1,27 @@
 package com.javaex.practice03;
+import java.util.*;
 
 public class Ex18 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요 : ");
+		a = sc.nextInt();
+		
+		for(int i= a-1; i>=0; i--){        
+			for(int j=0; j<=i;j++){
+			System.out.print("*");
+		}
+		System.out.println("");
+		}
+		
+		for(int i=1; i<=a; i++){        
+			for(int j=1; j<=i; j++){
+			System.out.print("*");
+		}
+		System.out.println("");
+		}
 	}
 
 }
